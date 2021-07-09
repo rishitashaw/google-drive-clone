e9a57f5f8a52e98841d1212aaf8ce86a2daad04f
Theseregrets <rishitashaw49@gmail.com>
2021-07-09 10:02:04 +0530

    firebase config added

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDrq783mDN4fRweGftw0SXFTzM6hX1hSn8",
+  authDomain: "drive-clone-745ef.firebaseapp.com",
+  projectId: "drive-clone-745ef",
+  storageBucket: "drive-clone-745ef.appspot.com",
+  messagingSenderId: "908063815259",
+  appId: "1:908063815259:web:e546c937396616ba78717e",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
